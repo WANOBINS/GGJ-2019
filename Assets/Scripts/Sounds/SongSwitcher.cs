be0ebf42c0e80019c2260c68b726fb8822bea46e
Jack <Jigglypuff100@gmail.com>
2019-01-27 10:47:06 -0500

    Changed Altar, House and gazebo prefabs

---
+++ Assets/Scripts/Sounds/SongSwitcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongSwitcher : MonoBehaviour
+{
+    public AudioSource songPlayer;
+
+    public AudioClip Demon;
+    public AudioClip Happy;
+
+    [Range(0, 1)]
+    public float DemonVolume = 0.5;
+    [Range(0, 1)]
+    public float HappyVolume = 0.5;
+
+    public GameObject ClosestStruc;
+    public float distance;
+    public bool DemonMusic = false;
+	// Use this for initialization
+	void Start ()
+    {
+        songPlayer = gameObject.AddComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(DemonMusic)
+        {
+
+            songPlayer.clip = Demon;
+            songPlayer.volume = DemonVolume;
+            if(songPlayer.isPlaying)
+            {
+                songPlayer.Play();
+            }
+            
+        }
+	}
+
+    void OnTriggerEnter(collider collision)
+    {
+        if(collision.gameObject.tag == "Altar")
+        {
+            if(Vector3.Distance(gameObject.transform, collision.gameObject.transform) < distance)
+            {
+                distance = Vector3.Distance(gameObject.transform, collision.gameObject.transform);
+                ClosestStruc = collision.gameObject;
+                DemonMusic = true;
+
+            }
+        }
+
+        if (collision.gameObject.tag == "House")
+        {
+            if (Vector3.Distance(gameObject.transform, collision.gameObject.transform) < distance)
+            {
+                distance = Vector3.Distance(gameObject.transform, collision.gameObject.transform);
+                ClosestStruc = collision.gameObject;
+                DemonMusic = false;
+
+            }
+        }
+    }
+}
