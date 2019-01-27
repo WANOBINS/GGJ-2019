d235dd119f0bbc813771b3ecaea95a0e7aa1a156
DaemonUmbra <daemon.umbra@gmail.com>
2019-01-27 13:32:24 -0500

    Updated Scripts

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Floor: MonoBehaviour
+    {
+        public void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.GetComponent<House>() != null || collision.gameObject.GetComponent<Altar>() != null || collision.gameObject.GetComponent<Gazebo>() != null)
+            {
+                collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            }
+        }
+    }
+}
