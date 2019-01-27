f10babb537577ed9e052d335fb6eaf3775444221
DaemonUmbra <daemon.umbra@gmail.com>
2019-01-27 12:33:46 -0500

    Updated GIblets Script

--- Assets/Scripts/LaserTarget.cs
+++ Assets/Scripts/LaserTarget.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LaserTarget : MonoBehaviour {
     GameObject BrokenHouse;
+    GameObject Gibs;
 
     private void Awake()
     {
         BrokenHouse = Resources.Load<GameObject>("Prefabs/BrokenHouse");
+        Gibs = Resources.Load<GameObject>("Prefabs/Giblets");
     }
 
     // Use this for initialization
@@ -22,8 +24,11 @@ public class LaserTarget : MonoBehaviour {
 
     public void LaserHit()
     {
-        GameObject brokenHouse = Instantiate(BrokenHouse, transform.position, transform.rotation);
-        Destroy(gameObject);
-        brokenHouse.GetComponent<Explosive>().Trigger();
+        if (gameObject.GetComponent<House>() != null)
+        {
+            GameObject brokenHouse = Instantiate(BrokenHouse, transform.position, transform.rotation);
+            Destroy(gameObject);
+            brokenHouse.GetComponent<Explosive>().Trigger();
+        }
     }
 }
